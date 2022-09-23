d639afa53b23bc49b14e0c340de349f64a8eadd8
kiady66 <kiady.raveloson@heavn.app>
2022-09-23 17:05:29 +0200

    fix(import): fix import error and update data reading from api

--- src/task.py
+++ src/task.py
@@ -1,76 +1,91 @@
 import json
-import login
-import send_response
-import update_db
+from src.login import login
+from src.send_response import send_response
+from src.request_db import request_db
+from src.request_db import update_db
+from src.static_function import convert_json_to_array
+from src.static_function import str_to_dtime
+from src.static_function import test
 
 
+# TODO: add flexible_id attribute to task in db
 def create_task(event, context):
-    if not login.check_user_token(event):
-        return send_response.send_response("access error", 403)
+    test("bonjour")
+
 
     # TODO: check if user own the task
+    # TODO: check if task has to be override
 
     try:
         insert_task_to_db(event)
-        send_response.send_response("success", 200)
+        return send_response("success", 200)
     except:
-        send_response.send_response("internal error", 500)
+        return send_response("internal error", 500)
 
 
 def update_task(event, context):
     if not login.check_user_token(event):
-        return send_response.send_response("access error", 403)
+        return send_response("access error", 403)
 
     # TODO: check if user own the task
+    # TODO: check if task has to be override (only warn front)
 
     try:
         update_task_to_db(event)
-        send_response.send_response("success", 200)
+        return send_response("success", 200)
     except:
-        send_response.send_response("internal error", 500)
+        return send_response("internal error", 500)
 
 
 def delete_task(event, context):
     if not login.check_user_token(event):
-        return send_response.send_response("access error", 403)
+        return send_response("access error", 403)
 
     # TODO: check if user own the task
+    # TODO: check if task has to be override (only warn front)
 
     try:
         delete_task_to_db(event)
-        send_response.send_response("success", 200)
+        send_response("success", 200)
     except:
-        send_response.send_response("internal error", 500)
+        send_response("internal error", 500)
 
 
 def insert_task_to_db(event):
     parameters = json.loads(event["body"])
     insert_script = ' INSERT INTO task (name, admins, users, type, configuration, priority, start_date, end_date, duration) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)'
     insert_value = (
-        parameters["name"], parameters["admin"],
-        parameters["users"], parameters["type"],
+        parameters["name"], convert_json_to_array(parameters["admins"]),
+        convert_json_to_array(parameters["users"]), parameters["type"],
         parameters["configuration"], parameters["priority"],
-        parameters["start_date"], parameters["end_date"],
+        str_to_dtime(parameters["start_date"]), str_to_dtime(parameters["end_date"]),
         parameters["duration"]
     )
-    update_db.update_db(insert_script, insert_value)
+    update_db(insert_script, insert_value)
 
 
 def update_task_to_db(event):
     parameters = json.loads(event["body"])
-    update_script = 'UPDATE task SET (name, admins, users, type, configuration, priority, start_date, end_date, duration) = (%s, %s, %s, %s, %s, %s, %s, %s, %s) where id = %s'
+    update_script = 'UPDATE task SET (name, admins, users, type, configuration, priority, start_date, end_date, duration, pinned) = (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) where id = %s'
     update_values = (
-        parameters["name"], parameters["admins"],
-        parameters["users"], parameters["type"],
+        parameters["name"], convert_json_to_array(parameters["admins"]),
+        convert_json_to_array(parameters["users"]), parameters["type"],
         parameters["configuration"], parameters["priority"],
-        parameters["start_date"], parameters["end_date"],
-        parameters["duration"], parameters["task_id"]
+        str_to_dtime(parameters["start_date"]), str_to_dtime(parameters["end_date"]),
+        parameters["duration"], convert_json_to_array(parameters["pinned"]), parameters["task_id"]
     )
-    update_db.update_db(update_script, update_values)
+    update_db(update_script, update_values)
 
 
 def delete_task_to_db(event):
     parameters = json.loads(event["body"])
     delete_script = 'DELETE FROM task where id = %s'
     delete_values = parameters["task_id"]
-    update_db.update_db(delete_script, delete_values)
+    request_db.update_db(delete_script, delete_values)
+
+
+def is_owner(event):
+    parameters = json.loads(event["body"])
+    request_script = 'SELECT * from task where id = %s AND %s IN users'
+    request_value = (parameters["task_id"], parameters["user_id"])
+    print(request_db(request_script, request_value))
