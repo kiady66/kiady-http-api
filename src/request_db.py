c99a1f108beab93e88528824d58680261b15c759
kiady66 <kiady.raveloson@heavn.app>
2022-09-23 15:26:46 +0200

    feat(db): make select request to database

--- src/request_db.py
+++ src/request_db.py
@@ -31,3 +31,28 @@ def update_db(insert_script, insert_value):
             cursor.close()
         if connection is not None:
             connection.close()
+
+
+def request_db(request_script, request_value):
+    try:
+        connection = psycopg2.connect(
+            host=hostname,
+            dbname=database,
+            user=username,
+            password=pwd,
+            port=port_id
+        )
+
+        cursor = connection.cursor()
+
+        cursor.execute(request_script, request_value)
+
+        return cursor.fetchall()
+
+    except Exception as error:
+        print(error)
+    finally:
+        if cursor is not None:
+            cursor.close()
+        if connection is not None:
+            connection.close()
