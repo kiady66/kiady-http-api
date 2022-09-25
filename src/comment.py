a6f5bd7c3978554fafb658a725d85b3101f7d66c
kiady66 <kiady.raveloson@heavn.app>
2022-09-25 15:48:53 +0200

    fix(task_updating): update multiple task at the same time

--- src/comment.py
+++ src/comment.py
@@ -1,5 +1,5 @@
 import json
-import update_db
+import request_db
 import send_response
 import login
 
@@ -47,18 +47,18 @@ def insert_comment_to_db(event, context):
     parameters = json.loads(event["body"])
     insert_script = 'INSERT INTO "comment" (sender, content, date) VALUES (%s, %s, %s)'
     insert_value = (parameters["sender"], parameters["content"], parameters["date"])
-    update_db.update_db(insert_script, insert_value)
+    request_db.update_db(insert_script, insert_value)
 
 
 def update_comment_to_db(event):
     parameters = json.loads(event["body"])
     update_script = 'UPDATE "comment" SET content = %s where id = %s'
     update_values = (parameters["content"], parameters["comment_id"])
-    update_db.update_db(update_script, update_values)
+    request_db.update_db(update_script, update_values)
 
 
 def delete_comment_to_db(event):
     parameters = json.loads(event["body"])
     delete_script = 'DELETE FROM "comment" where id = %s'
     delete_values = parameters["comment_id"]
-    update_db.update_db(delete_script, delete_values)
+    request_db.update_db(delete_script, delete_values)
--- src/task.py
+++ src/task.py
@@ -5,17 +5,15 @@ from src.request_db import request_db
 from src.request_db import update_db
 from src.static_function import convert_json_to_array
 from src.static_function import str_to_dtime
-from src.static_function import test
-
+from src.static_function import extract_task_from_request
 
 # TODO: add flexible_id attribute to task in db
 def create_task(event, context):
-
     # TODO: check if user own the task
     # TODO: check if task has to be override
 
     try:
-        insert_task_to_db(event)
+        insert_task_to_db(extract_task_from_request(event))
         return send_response("success", 200)
     except:
         return send_response("internal error", 500)
@@ -29,7 +27,7 @@ def update_task(event, context):
     # TODO: check if task has to be override (only warn front)
 
     try:
-        update_task_to_db(event)
+        update_task_to_db(extract_task_from_request(event))
         return send_response("success", 200)
     except:
         return send_response("internal error", 500)
@@ -43,43 +41,43 @@ def delete_task(event, context):
     # TODO: check if task has to be override (only warn front)
 
     try:
-        delete_task_to_db(event)
+        delete_task_to_db(extract_task_from_request(event))
         send_response("success", 200)
     except:
         send_response("internal error", 500)
 
 
-def insert_task_to_db(event):
-    parameters = json.loads(event["body"])
-    insert_script = ' INSERT INTO task (name, admins, users, type, configuration, priority, start_date, end_date, duration) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)'
-    insert_value = (
-        parameters["name"], convert_json_to_array(parameters["admins"]),
-        convert_json_to_array(parameters["users"]), parameters["type"],
-        parameters["configuration"], parameters["priority"],
-        str_to_dtime(parameters["start_date"]), str_to_dtime(parameters["end_date"]),
-        parameters["duration"]
-    )
-    update_db(insert_script, insert_value)
+def insert_task_to_db(tasks):
+    for task in tasks:
+        insert_script = ' INSERT INTO task (name, admins, users, type, configuration, priority, start_date, end_date, duration) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)'
+        insert_value = (
+            task["name"], convert_json_to_array(task["admins"]),
+            convert_json_to_array(task["users"]), task["type"],
+            task["configuration"], task["priority"],
+            str_to_dtime(task["start_date"]), str_to_dtime(task["end_date"]),
+            task["duration"]
+        )
+        update_db(insert_script, insert_value)
 
 
-def update_task_to_db(event):
-    parameters = json.loads(event["body"])
-    update_script = 'UPDATE task SET (name, admins, users, type, configuration, priority, start_date, end_date, duration, pinned) = (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) where id = %s'
-    update_values = (
-        parameters["name"], convert_json_to_array(parameters["admins"]),
-        convert_json_to_array(parameters["users"]), parameters["type"],
-        parameters["configuration"], parameters["priority"],
-        str_to_dtime(parameters["start_date"]), str_to_dtime(parameters["end_date"]),
-        parameters["duration"], convert_json_to_array(parameters["pinned"]), parameters["task_id"]
-    )
-    update_db(update_script, update_values)
+def update_task_to_db(tasks):
+    for task in tasks:
+        update_script = 'UPDATE task SET (name, admins, users, type, configuration, priority, start_date, end_date, duration, pinned) = (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) where id = %s'
+        update_values = (
+            task["name"], convert_json_to_array(task["admins"]),
+            convert_json_to_array(task["users"]), task["type"],
+            task["configuration"], task["priority"],
+            str_to_dtime(task["start_date"]), str_to_dtime(task["end_date"]),
+            task["duration"], convert_json_to_array(task["pinned"]), task["task_id"]
+        )
+        update_db(update_script, update_values)
 
 
-def delete_task_to_db(event):
-    parameters = json.loads(event["body"])
-    delete_script = 'DELETE FROM task where id = %s'
-    delete_values = parameters["task_id"]
-    request_db.update_db(delete_script, delete_values)
+def delete_task_to_db(tasks):
+    for task in tasks:
+        delete_script = 'DELETE FROM task where id = %s'
+        delete_values = task["task_id"]
+        request_db.update_db(delete_script, delete_values)
 
 
 def is_owner(tasks, user_id):
@@ -89,3 +87,24 @@ def is_owner(tasks, user_id):
         if not request_db(request_script, request_value):
             return False
     return False
+
+
+# return list of task that overlap
+def check_overlaps(tasks):
+    request_script = 'SELECT * FROM task where '
+    request_value = ()
+    length = len(tasks) - 1
+    i = 0
+
+    for task in tasks:
+        request_script = request_script + 'start_date between %s and %s or end_date between %s and %s'
+        request_value = request_value + (
+            str_to_dtime(task["start_date"]),
+            str_to_dtime(task["end_date"]),
+            str_to_dtime(task["start_date"]),
+            str_to_dtime(task["end_date"])
+        )
+        if i < length:
+            request_script = request_script + 'or '
+
+    return request_db(request_script, request_value)
