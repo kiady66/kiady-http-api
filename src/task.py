0a2031a1dd4179272362b86e111d1e3608306a70
kiady66 <kiady.raveloson@heavn.app>
2022-09-25 16:14:17 +0200

    chore(reformat): remove duplicated code

--- src/task.py
+++ src/task.py
@@ -1,5 +1,5 @@
 import json
-from src.login import login
+from src.login import check_user_token
 from src.send_response import send_response
 from src.request_db import request_db
 from src.request_db import update_db
@@ -7,44 +7,18 @@ from src.static_function import convert_json_to_array
 from src.static_function import str_to_dtime
 from src.static_function import extract_task_from_request
 
+
 # TODO: add flexible_id attribute to task in db
 def create_task(event, context):
-    # TODO: check if user own the task
-    # TODO: check if task has to be override
-
-    try:
-        insert_task_to_db(extract_task_from_request(event))
-        return send_response("success", 200)
-    except:
-        return send_response("internal error", 500)
+    update_tasks(event, insert_task_to_db)
 
 
 def update_task(event, context):
-    if not login.check_user_token(event):
-        return send_response("access error", 403)
-
-    # TODO: check if user own the task
-    # TODO: check if task has to be override (only warn front)
-
-    try:
-        update_task_to_db(extract_task_from_request(event))
-        return send_response("success", 200)
-    except:
-        return send_response("internal error", 500)
+    update_tasks(event, update_task_to_db)
 
 
 def delete_task(event, context):
-    if not login.check_user_token(event):
-        return send_response("access error", 403)
-
-    # TODO: check if user own the task
-    # TODO: check if task has to be override (only warn front)
-
-    try:
-        delete_task_to_db(extract_task_from_request(event))
-        send_response("success", 200)
-    except:
-        send_response("internal error", 500)
+    update_tasks(event, delete_task_to_db, True)
 
 
 def insert_task_to_db(tasks):
@@ -108,3 +82,25 @@ def check_overlaps(tasks):
             request_script = request_script + 'or '
 
     return request_db(request_script, request_value)
+
+
+# delete_call_back is none if update_tasks is not a deleting function
+def update_tasks(event, callback, is_deleting_task=False):
+    if not check_user_token(event):
+        return send_response("access error", 403)
+
+    if not is_owner(extract_task_from_request(event)):
+        return send_response("access error", 403)
+
+    if not is_deleting_task:
+        overlapping = check_overlaps(extract_task_from_request(event))
+        if overlapping:
+            return send_response({
+                "tasks_overlapping": overlapping,
+            }, 200)
+
+    try:
+        callback(extract_task_from_request(event))
+        send_response("success", 200)
+    except:
+        send_response("internal error", 500)
